cc01629f3987c7e4edbc0c6b22116b0f969f2b06
sanfit <sanfit@vk.com>
2024-06-09 16:21:05 +0300

    update pydantic-settings

--- src/database.py
+++ src/database.py
@@ -13,8 +13,8 @@ class Base(DeclarativeBase):
     __abstract__ = True
 
     @declared_attr.directive
-    def __tablename__(cls) -> str:
-        return cls.__name__.lower() + "s"
+    def __tablename__(self) -> str:
+        return self.__name__.lower() + "s"
 
     id: Mapped[int] = mapped_column(primary_key=True, type_=BigInteger)
 
